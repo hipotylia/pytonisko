17dab9f9812bcde83fdf87b7c1457db822da02fa
Gerard Olenski <gerard.olenski@gmail.com>
2024-01-25 20:20:03 +0100

    Task1: run me and please fix me

---
+++ calc.py
@@ -0,0 +1,28 @@
+def add(_a: float, _b: float) -> float:
+    return 0
+
+
+def subtract(_a: float, _b: float) -> float:
+    return 0
+
+
+def multiply(_a: float, _b: float) -> float:
+    return 0
+
+
+def divide(_a: float, _b: float) -> float:
+    return 0
+
+
+def power(_a: int, _b: int) -> int:
+    return 0
+
+
+print('Hi')
+print('Look - I can calculate :)')
+print(f'10 + 15 = {add(10, 15)}')
+print(f'30 - 15 = {subtract(30, 15)}')
+print(f'5 * 9 = {multiply(5, 8)}')
+print(f'42 / 7 = {divide(42, 7)}')
+print(f'3 ^ 2 = {power(3, 2)}')
+print('Everything is OK, right?')
