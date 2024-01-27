174097860f5c0f669218477c51d8a7b2693fc810
hipotylia <zosia.ole211@gmail.com>
2024-01-27 11:53:48 +0100

    fixed issues

--- calc.py
+++ calc.py
@@ -18,6 +18,10 @@ def power(_a: int, _b: int) -> int:
     return _a ** _b
 
 
+def input_num(_txt: str) -> float:
+    return float(input(_txt))
+
+
 print('Hi')
 print('Look - I can calculate :)')
 print(f'10 + 15 = {add(10, 15)}')
@@ -30,19 +34,18 @@ print()
 
 print('Now I will calculate on difrent numbers, and..')
 print('..its gonna be numbers with decimals!')
-print('15.67 + 6.789 = ', add(_a=15.67, _b=6.789 ))
-print('4.12 - 10.555 = ', subtract(_a=4.12, _b=10.555))
-print('2.5 * 3.8 = ', multiply(_a=2.5, _b=3.8))
-print('100.23 / 3.12 =', divide(_a=100.23, _b=3.12))
+print('15.67 + 6.789 = ', add(15.67, 6.789))
+print('4.12 - 10.555 = ', subtract(4.12, 10.555))
+print('2.5 * 3.8 = ', multiply(2.5, 3.8))
+print('100.23 / 3.12 =', divide(100.23, 3.12))
 print()
-
-num1 = float(input('Write first number: '))
-num2 = float(input('Write second number: '))
 
 print('I can do also one thing. I think you gonna be suprised ;)')
 print("i'm gonna calculate any number that you want! If you want to see this just give me numbers to count")
-print( num1, '+', num2, '=', add(_a=num1, _b=num2))
-print( num1, '-', num2, '=', subtract(_a=num1, _b=num2))
-print( num1, '*', num2, '=', multiply(_a=num1, _b=num2))
-print( num1, ':', num2, '=', divide(_a=num1, _b=num2))
+num1 = input_num('Input first number: ')
+num2 = input_num('Input second number: ')
+print(num1, '+', num2, '=', add(num1, num2))
+print(num1, '-', num2, '=', subtract(num1, num2))
+print(num1, '*', num2, '=', multiply(num1, num2))
+print(num1, ':', num2, '=', divide(num1, num2))
 print('I hope you like it! Have a nice day:D')
